2866b4ed344a0dac1ff854df9990b66b62fceaa6
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-07 14:23:38 +0530

    added use case 4

--- Program.cs
+++ Program.cs
@@ -1,31 +1,30 @@
-﻿using System;
-
-namespace DayThreeEmployeeWage
+﻿namespace DayThreeEmployeeWage
 {
     class Program
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
-            int IS_FULL_TIME = 2;
-            int IS_PART_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             int empWage, empHrs;
 
-            if (empCheck == IS_FULL_TIME)
+            switch(empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
