b1797e8cbd252d35d119391525f468550624740d
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-07 14:11:18 +0530

    added use case 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DayThreeEmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            int IS_PRESENT = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
