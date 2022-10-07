eaea4938d68cc70147ad245b6bba91a816314b80
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-07 14:28:50 +0530

    added use case 6

--- Program.cs
+++ Program.cs
@@ -6,15 +6,16 @@
         public const int IS_PART_TIME = 1;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUMBER_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_A_MONTH = 100;
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
 
             Random random = new Random();
-            int empWage = 0, empHrs = 0, totalEmpWage = 0;
+            int empWage = 0, empHrs = 0, totalEmpWage = 0, totalWorkingHrs = 0, day = 0;
 
-            for (int day = 0; day < NUMBER_OF_WORKING_DAYS; day++)
+            while (totalWorkingHrs <= MAX_HRS_IN_A_MONTH && day < NUMBER_OF_WORKING_DAYS)
             {
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -32,7 +33,11 @@
                 empWage = empHrs * EMP_RATE_PER_HOUR;
                 totalEmpWage += empWage;
                 Console.WriteLine("Daily employee wage: " + empWage);
+                totalWorkingHrs += empHrs;
+                day++;
             }
+            Console.WriteLine("Total working hours in the month: {0}", totalWorkingHrs);
+            Console.WriteLine("Total number of working days: {0}", day);
             Console.WriteLine("Total employee wage for the month is: {0}", totalEmpWage);
 
         }
